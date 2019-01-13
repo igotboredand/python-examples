bdd916bfa10793e1fa8be3fc0bbdd0446b1d2ee1
igotboredand <46539368+igotboredand@users.noreply.github.com>
2019-01-13 08:23:31 -0800

    Added elasticsearch csv import script
    Script takes two parameters: the csv file path and the elasticsearch index.
    Usage is noted in the file.
    Needs some work on error handling

---
+++ examples/elasticsearch/csv-import.py
@@ -0,0 +1,19 @@
+from elasticsearch import helpers, Elasticsearch
+import csv
+import sys
+
+
+# Usage python csv-import <path to csv file> <name of index in elasticsearch>
+
+#TODO error handling around lack of input
+csv_file = sys.argv[1]
+index_name = sys.argv[2]
+
+
+es = Elasticsearch()
+
+# Encoding should be UTF-8, use ISO-8859-1 if you use fancy characters
+
+with open(csv_file,encoding="UTF-8") as f:
+    reader = csv.DictReader(f)
+    helpers.bulk(es, reader, index=index_name, doc_type='my-type')
